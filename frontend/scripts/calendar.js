e1b758f2366a9ce5c01422f0318eed395480b2a8
Iicytower <szymonfirkowicz@gmail.com>
2024-11-15 15:08:19 +0100

    feat: add getMonthWithFullWeeks functionality

--- frontend/scripts/calendar.js
+++ frontend/scripts/calendar.js
@@ -1,3 +1,4 @@
+import { getSliceOfObject, incrementDayInDateString } from './helpers.js';
 import { readData } from './readData.js';
 
 export class Calendar {
@@ -30,5 +31,43 @@ export class Calendar {
     if(!result) throw new Error('wrong date');
 
     return result;
+  }
+
+  getMonthWithFullWeeks(date) {
+    if (!date) throw new Error('wrong date');
+
+    if (date instanceof Date) {
+      date = date.toISOString().split('T')[0];
+    }
+
+    const karajDate = this.getKarajDate(date);
+
+    const { firstDayOfKarajMonth, lastDayOfKarajMonth } = this._getFirstAndLastDayOfKarajMonth(karajDate);
+
+    const weekDayOfFirstDayOfMonth = this._karajDetails[firstDayOfKarajMonth].weekDay;
+    const weekDayOfLastDayOfMonth = this._karajDetails[lastDayOfKarajMonth].weekDay;
+
+    const month = getSliceOfObject(
+      this._karajDetails,
+      { startKey: firstDayOfKarajMonth, minusCount: weekDayOfFirstDayOfMonth - 1 },
+      { endKey: lastDayOfKarajMonth, plusCount: 7 - weekDayOfLastDayOfMonth }
+    );
+
+    return Object.entries(month).reduce((acc, [karajDate, karajDateData]) => {
+      acc.push({...karajDateData, karajDate, gregDate: this._karajToGreg[karajDate]});
+      return acc;
+    }, []);
+  }
+
+  _getFirstAndLastDayOfKarajMonth(date) {
+    let firstDayOfKarajMonth = date.replace(/(\d{2})$/, '01');
+
+    let lastDayOfKarajMonth = date.replace(/(\d{2})$/, '30');
+
+    if(!this._karajDetails[lastDayOfKarajMonth]) {
+      lastDayOfKarajMonth = lastDayOfKarajMonth.replace(/(\d{2})$/, '29')
+    }
+
+    return { firstDayOfKarajMonth, lastDayOfKarajMonth };
   }
 }
--- frontend/scripts/helpers.js
+++ frontend/scripts/helpers.js
@@ -1,3 +1,27 @@
 export function verifyDate(date) {
   return /\d{4}-\d{2}-\d{2}/gm.test(date);
 }
+
+export function incrementDayInDateString(date) {
+  const newDate = date.replace(/(\d{4})-(\d{2})-(\d{2})/, (match, year, month, day) => {
+    const incrementedDay = String(parseInt(day, 10) + 1).padStart(2, '0');
+    return `${year}-${month}-${incrementedDay}`;
+  });
+
+  return newDate;
+}
+
+export function getSliceOfObject(obj, { startKey, minusCount = 0 }, { endKey, plusCount = 0 }) {
+  const keys = Object.keys(obj);
+  const startIndex = keys.indexOf(startKey) - minusCount;
+  const endIndex = keys.indexOf(endKey) + plusCount;
+  
+  const objectSlice = {};
+  for (let i = startIndex; i <= endIndex; i++) {
+    const key = keys[i];
+    objectSlice[key] = obj[key];
+
+  }
+  
+  return objectSlice;
+}
