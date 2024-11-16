c5ca86097b28991ba33c0217e7bf7efae36d9b67
Iicytower <szymonfirkowicz@gmail.com>
2024-11-16 19:27:56 +0100

    feat: change colors and font sizes, update search date input

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -6,49 +6,24 @@ const gregToKarajBtn = document.querySelector('#gregToKarajBtn');
 const karajToGregBtn = document.querySelector('#karajToGregBtn');
 const nextMonthBtn = document.querySelector("#nextMonth");
 const previousMonthBtn = document.querySelector("#previousMonth");
-const goToDate = document.querySelector('#goToDate');
-const monthName = document.querySelector('#monthName');
-
+const goToDateInput = document.querySelector('#goToDateInput');
+const goToDateBtn = document.querySelector('#goToDateBtn');
+const monthNameElement = document.querySelector('#monthName');
 const gregToKarajInput = document.querySelector("#gregToKarajInput");
 const karajToGregInput = document.querySelector("#karajToGregInput");
 
 let calendarDb;
 let currentMonthInView;
+
 window.onload = async () => {
   calendarDb = await Calendar.init();
 
-  const currentMonthCalendarData = calendarDb.getMonthWithFullWeeks(new Date());
+  const currentMonthCalendarData = calendarDb.getMonthWithFullWeeks();
 
-  insertCalendar(currentMonthCalendarData, monthName);
+  insertCalendar(currentMonthCalendarData, monthNameElement);
 
   currentMonthInView = getFirstDayOfMonth(currentMonthCalendarData[9].karajDate);
 }
-
-goToDate.addEventListener('keydown', (event) => {
-  if(event.key != 'Enter') {
-    return;
-  }
-
-  const calendarData = calendarDb.getMonthWithFullWeeks(goToDate.value);
-  insertCalendar(calendarData, monthName);
-  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
-});
-
-previousMonthBtn.addEventListener('click', () => {
-  const firstDayOfPreviousMonth = calendarDb.getFirstDayOfPreviousMonth(currentMonthInView);
-
-  const calendarData = calendarDb.getMonthWithFullWeeks(calendarDb.getGregDate(firstDayOfPreviousMonth));
-  insertCalendar(calendarData, monthName);
-  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
-});
-
-nextMonthBtn.addEventListener('click', () => {
-  const firstDayOfNextMonth = calendarDb.getFirstDayOfNextMonth(currentMonthInView);
-
-  const calendarData = calendarDb.getMonthWithFullWeeks(calendarDb.getGregDate(firstDayOfNextMonth));
-  insertCalendar(calendarData, monthName);
-  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
-});
 
 const gregToKarajCallback = (event) => {
   if(event.type == 'keydown' && event.key != 'Enter') {
@@ -93,7 +68,41 @@ const karajToGregCallback = (event) => {
   }
 }
 
+const goToDateCallback = (event) => {
+  if(event.type == 'keydown' && event.key != 'Enter') {
+    return;
+  }
+
+  const gregOrKaraj = (Number(goToDateInput.value.split('-')[0]) > 5000) ? 'KARAJ' : 'GREG';
+
+  let dateToSearch = (gregOrKaraj === 'KARAJ') ? calendarDb.getGregDate(goToDateInput.value) : goToDateInput.value;
+
+  const calendarData = calendarDb.getMonthWithFullWeeks(dateToSearch);
+  insertCalendar(calendarData, monthNameElement);
+  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
+}
+
+const previousMonthCallback = () => {
+  const firstDayOfPreviousMonth = calendarDb.getFirstDayOfPreviousMonth(currentMonthInView);
+
+  const calendarData = calendarDb.getMonthWithFullWeeks(calendarDb.getGregDate(firstDayOfPreviousMonth));
+  insertCalendar(calendarData, monthNameElement);
+  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
+}
+
+const nextMonthCallback = () => {
+  const firstDayOfNextMonth = calendarDb.getFirstDayOfNextMonth(currentMonthInView);
+
+  const calendarData = calendarDb.getMonthWithFullWeeks(calendarDb.getGregDate(firstDayOfNextMonth));
+  insertCalendar(calendarData, monthNameElement);
+  currentMonthInView = getFirstDayOfMonth(calendarData[9].karajDate);
+}
+
 gregToKarajBtn.addEventListener('click', gregToKarajCallback);
 gregToKarajInput.addEventListener('keydown', gregToKarajCallback);
 karajToGregBtn.addEventListener('click', karajToGregCallback);
 karajToGregInput.addEventListener('keydown', karajToGregCallback);
+goToDateInput.addEventListener('keydown', goToDateCallback);
+goToDateBtn.addEventListener('click', goToDateCallback);
+previousMonthBtn.addEventListener('click', previousMonthCallback);
+nextMonthBtn.addEventListener('click', nextMonthCallback);
