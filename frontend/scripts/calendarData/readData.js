09e167886a4e0c2ef8fb17589dbc806ad6457cd4
Iicytower <szymonfirkowicz@gmail.com>
2024-12-16 16:26:25 +0100

    refactor: remove nn code

--- frontend/scripts/calendarData/readData.js
+++ frontend/scripts/calendarData/readData.js
@@ -5,9 +5,5 @@ export async function readData() {
   const karajToGreg = await readJSONFile(`https://raw.githubusercontent.com/Iicytower/karaj-calendar/main/frontend/data/karajToGreg.json`);
   const karajDetails = await readJSONFile(`https://raw.githubusercontent.com/Iicytower/karaj-calendar/main/frontend/data/karajDetails.json`);
 
-  // const gregToKaraj = await readJSONFile('../data/gregToKaraj.json');
-  // const karajToGreg = await readJSONFile('../data/karajToGreg.json');
-  // const karajDetails = await readJSONFile('../data/karajDetails.json');
-
   return { gregToKaraj, karajToGreg, karajDetails };
 }
