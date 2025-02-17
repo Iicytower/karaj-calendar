c3f1d08afb4c9cc64e4bfbcfed997af4abc8bc41
Szymon <szymonfirkowicz@gmail.com>
2025-02-17 18:13:34 +0100

    Develop (#28)
    * fix: last fixes. part2
    
    * fix: sources handling for articles in popup
    
    * fix: add scrollable to article. update menuItems.json
    
    * fix: update menuItems, update menu genration
    
    * update menuitems
    
    * update menu items
    
    * update: menuItems
    
    * update: menuItems finish polish texts
    
    * update: menuItems update names and order
    
    * feat: Feliks visual changes
    
    * update: menuItems update
    
    * fix: background-size for background image
    
    * update: background image, menu items
    
    * update: menu items
    
    * feat: add gh actions
    
    * refactor: add favicon link in index.html
    
    * refactor: change paths to json files
    
    * refactor: remove gh actions
    
    * test
    
    * remove gh actions
    
    * update path in gh pages
    
    * add favicon to different place
    
    * add favicon def into index.html
    
    * update: menu items
    
    * feat: remove scroll with touch, add arrows on phones
    
    * feat: fix goto form
    
    * update: menu items
    
    * feat: change background
    
    * feat: update menu items
    
    * feat: change arrows entity
    
    * update: menu items
    
    * refactor: add helper func
    
    * refactor: fix some styles, update design
    
    * fix: arrows. part1
    
    * feat: remove svg files
    
    * fix: scrolling up always on article
    
    * fix: font-sizes, background-image
    
    * feat: add read_plain_kokizov.ts script
    
    * remove mistake
    
    * update: menu items
    
    * feat: adjust styles
    
    * feat: disable menu button
    
    * fix: last things before release
    
    * feat: add marked choosen language
    
    * feat: change haggada to haggada kieciasi
    
    * feat: add years range in search bar
    
    * fix: add spaces in years rande
    
    ---------
    
    Co-authored-by: szymek <szymek@localhost.localdomain>

--- frontend/scripts/createGoToForm.js
+++ frontend/scripts/createGoToForm.js
@@ -141,7 +141,7 @@ export function createGoToForm(calendarDb) {
   container.classList.add('goToForm');
 
   const yearsRange = document.createElement('span');
-  yearsRange.innerText = '5757(1997)-5861(2101)';
+  yearsRange.innerText = '5757(1997) - 5861(2101)';
   yearsRange.style.marginBottom = '3px';
 
   container.appendChild(yearsRange);
